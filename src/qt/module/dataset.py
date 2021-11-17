aca1f4544a76df2965a70f1d08ef56673535d423
ruiqingluo <ruiqingluo1@163.com>
2021-11-17 21:07:53 +0800

    # update

--- src/qt/module/dataset.py
+++ src/qt/module/dataset.py
@@ -4,14 +4,24 @@ from torch.utils.data import Dataset
 
 
 def extractData(path):
-    data = []
     df = pd.read_csv(path, dtype={"代码": "category"})
     df = df[df['日期'] >= 20110131]
     df = df[df['日期'] <= 20200529]
-    codes = df.groupby('代码').groups.keys()
+    data_list = {'code': df['代码'], 'date': df['日期'], 'return': df['10日回报率'], 'open': df['开盘价(元)'], 'close': df['收盘价(元)'],
+            'high': df['最高价(元)'], 'low': df['最低价(元)'], 'volume': df['成交量(股)'], 'vwap': df['vwap/high'] * df['最高价(元)'],
+            'turn': df['换手率(%)'], 'free_turn': df['换手率(基准.自由流通股本)'], 'label': df['涨跌幅(%)']}
+    data = pd.DataFrame(data_list)
+
+    # Normalization By Feature
+    mean = data.mean(axis=0)
+    std = data.std(axis=0)
+    norm_data = (data - mean) / std
+
+    codes = norm_data.groupby('code').groups.keys()
+    data_arr = []
     for code in codes:
-        data.append(df[df['代码'] == code])
-    return data
+        data_arr.append(norm_data[norm_data['code'] == code])
+    return data_arr
 
 
 class Securities(Dataset):
@@ -29,20 +39,20 @@ class Securities(Dataset):
         valid_labels = []
         valid_data = []
         for sub_data in org_data:
-            sub_data.sort_values(by='日期', inplace=True)
-            sub_data = sub_data.iloc[:, 2: 18].to_numpy(dtype='float32')  # (23400, 16)
-            nums, self.height = sub_data.shape
-            # Labels 预测目标为5天后的涨跌幅
-            sub_labels = sub_data[:, 5]
-            for i in range(self.width + 4, nums):
-                start = i - self.width - 4
-                end = i - 4
-                if i % 2 is 0:
+            sub_data.sort_values(by='date', inplace=True)
+            feats_data = sub_data.iloc[:, 2: 11].to_numpy(dtype='float32')  # (23400, 9)
+            nums, _ = feats_data.shape
+            # Labels 预测目标为10天后的涨跌幅
+            sub_labels = sub_data.iloc[:, 11].to_numpy(dtype='float32')
+            for i in range(self.width + 9, nums):
+                start = i - self.width - 9
+                end = i - 9
+                if i % 2 is not 0:
                     training_labels.append(sub_labels[i])
-                    training_data.append(sub_data[start: end])
+                    training_data.append(feats_data[start: end])
                 else:
                     valid_labels.append(sub_labels[i])
-                    valid_data.append(sub_data[start: end])
+                    valid_data.append(feats_data[start: end])
 
         if mode == 'train':
             self.labels = training_labels
@@ -53,7 +63,7 @@ class Securities(Dataset):
 
     def __getitem__(self, index):
         data_img = self.data[index]
-        data_img = data_img.reshape(-1, 30, 16).transpose((0, 2, 1))  # (1, 16, 30)
+        data_img = data_img.reshape(-1, 30, 9).transpose((0, 2, 1))  # (1, 9, 30)
         data_label = self.labels[index]
         return data_img, np.array([data_label])
 
--- src/qt/test.py
+++ src/qt/test.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
-import pandas as pd
 from alphanet import AlphaNet
+from module.component import Cov, Corr, StdDev, ZScore, Return, DecayLinear, Pooling
+import pandas as pd
 
 
 # df = pd.read_csv('data/data.csv', header=None)
@@ -12,12 +13,22 @@ from alphanet import AlphaNet
 # model = AlphaNet()
 #
 # pred = model(t_data)
+
+
 path = './data/test_data.zip'
 df = pd.read_csv(path, dtype={"代码": "category"})
 # org_data = df.iloc[:, 2: 18].to_numpy(dtype='float64')  # (23400, 16)
 
-# df = df[df['日期'] >= 20110131]
-# df = df[df['日期'] <= 20200529]
+df = df[df['日期'] >= 20110131]
+df = df[df['日期'] <= 20200529]
 # df.groupby('代码').count()
+# df.sort_values(by=['代码', '日期'], inplace=True)
 
-df.sort_values(by=['代码', '日期'], inplace=True)
+# x = pd.read_csv('x.txt', header=None, sep=' ')
+# x = x.to_numpy()
+# x_t = x.reshape(1, 1, 9, 30)
+# t = torch.tensor(x_t)
+#
+# cov = Cov(2, 30, 10)
+# c1 = cov(t[:, :, :2, :])
+# c2 = cov(t[:, :, [0, 5], :])
